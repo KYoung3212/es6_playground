a8018db447ab221d25c3111eb6f2e7e2ef71e1fc
kyoung3212 <kevin.young3212@gmail.com>
2018-07-26 15:51:59 -0700

    Completed class DEMO

--- app/index.js
+++ app/index.js
@@ -1,1 +1,27 @@
-console.log('App working: open app/index.js to start exploring ES6');
+import {Person, SuperPerson} from './person';
+const bob = new Person('Bob');
+console.log(bob.speak('My name is Bob!'))
+
+const superSarah = new SuperPerson('Sarah', 'Tip to real quiet');
+superSarah.speak('Hi I\'m super Sarah');
+superSarah.saySuperPower();
+// class Person {
+//     constructor(name){
+//         this.name=name
+//     }
+//     speak(text){
+//         return `${this.name} Says: ${text}`
+//     }
+// }
+
+
+// import elementToBody from './add_to_dom';
+
+// elementToBody('h1', 'Something Different');
+
+// elementToBody('p', 'Here is a second element added to Body');
+// $('body').append('<h1>I came from jQuery!</h1>');
+
+// const header = document.createElement('h1');
+// header.innerText = 'This was dynamically created';
+// document.body.appendChild(header);
